39053548ee5a8529ea14ec7d6e854a4bde00dc84
EastOrb <102253515+EastOrb@users.noreply.github.com>
2023-09-01 09:51:31 +0100

    Update index.ts
    I've defined error messages as constants in the ERRORS object to make them easier to manage.
    
    I've improved error handling by explicitly checking for null when accessing the ticket in the getTicket, reserveTicket, and deleteTicket functions. This prevents potential null reference errors.

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,15 @@ type MovieTicket = Record<{
 
 const ticketStorage = new StableBTreeMap<string, MovieTicket>(0, 44, 1024);
 
+// Error Messages
+const ERRORS = {
+    EMPTY_MOVIE_NAME: 'Movie name cannot be empty.',
+    INVALID_SEAT: 'Invalid seat number.',
+    TICKET_NOT_FOUND: (id: string) => `Ticket with id=${id} not found.`,
+    TICKET_ALREADY_RESERVED: 'This ticket is already reserved.',
+    TICKET_DOES_NOT_EXIST: (id: string) => `Ticket with id=${id} does not exist.`,
+};
+
 $query;
 export function getAllTickets(): Result<Vec<MovieTicket>, string> {
     return Result.Ok(ticketStorage.values());
@@ -19,10 +28,12 @@ export function getAllTickets(): Result<Vec<MovieTicket>, string> {
 
 $query;
 export function getTicket(id: string): Result<MovieTicket, string> {
-    return match(ticketStorage.get(id), {
-        Some: (ticket: MovieTicket) => Result.Ok<MovieTicket, string>(ticket),
-        None: () => Result.Err<MovieTicket, string>(`Ticket with id=${id} not found.`)
-    });
+    const ticket = ticketStorage.get(id);
+    if (ticket !== null) {
+        return Result.Ok(ticket);
+    } else {
+        return Result.Err(ERRORS.TICKET_NOT_FOUND(id));
+    }
 }
 
 $update;
@@ -32,10 +43,10 @@ export function addTicket(movie: string, seat: nat64): Result<MovieTicket, strin
 
     // Validate inputs
     if (movie.trim() === '') {
-        return Result.Err<MovieTicket, string>('Movie name cannot be empty.');
+        return Result.Err(ERRORS.EMPTY_MOVIE_NAME);
     }
     if (seat <= 0) {
-        return Result.Err<MovieTicket, string>('Invalid seat number.');
+        return Result.Err(ERRORS.INVALID_SEAT);
     }
 
     const newTicket: MovieTicket = { 
@@ -51,23 +62,25 @@ export function addTicket(movie: string, seat: nat64): Result<MovieTicket, strin
 
 $update;
 export function reserveTicket(id: string): Result<MovieTicket, string> {
-    return match(ticketStorage.get(id), {
-        Some: (ticket: MovieTicket) => {
-            if (ticket.reserved) {
-                return Result.Err<MovieTicket, string>('This ticket is already reserved.');
-            }
-            const reservedTicket: MovieTicket = { ...ticket, reserved: true };
-            ticketStorage.insert(ticket.id, reservedTicket);
-            return Result.Ok(reservedTicket);
-        },
-        None: () => Result.Err<MovieTicket, string>(`Ticket with id=${id} does not exist.`)
-    });
+    const ticket = ticketStorage.get(id);
+    if (ticket !== null) {
+        if (ticket.reserved) {
+            return Result.Err(ERRORS.TICKET_ALREADY_RESERVED);
+        }
+        const reservedTicket: MovieTicket = { ...ticket, reserved: true };
+        ticketStorage.insert(ticket.id, reservedTicket);
+        return Result.Ok(reservedTicket);
+    } else {
+        return Result.Err(ERRORS.TICKET_DOES_NOT_EXIST(id));
+    }
 }
 
 $update;
 export function deleteTicket(id: string): Result<MovieTicket, string> {
-    return match(ticketStorage.remove(id), {
-        Some: (deletedTicket: MovieTicket) => Result.Ok<MovieTicket, string>(deletedTicket),
-        None: () => Result.Err<MovieTicket, string>(`Ticket with id=${id} does not exist.`)
-    });
+    const deletedTicket = ticketStorage.remove(id);
+    if (deletedTicket !== null) {
+        return Result.Ok(deletedTicket);
+    } else {
+        return Result.Err(ERRORS.TICKET_DOES_NOT_EXIST(id));
+    }
 }
